879535d127c2bd794075ef43d2ecca0ee9971311
ClaudioQD <claudio.quijada.huawei@gmail.com>
2022-04-07 01:56:03 -0400

    agregue toltit

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,3 +45,9 @@ $("#direccionarContacto").click(function() {
         scrollTop: $("#contacto").offset().top
     }, 2000);
 });
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
