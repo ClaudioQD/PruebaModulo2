09c5a37c37fc2e4944245c71a883b0bbd4dcca28
ClaudioQD <claudio.quijada.huawei@gmail.com>
2022-04-07 21:48:07 -0400

    corregido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,26 @@
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
 $("#titulo1").click(function () {
-    $("#parrafo1").toggle("slow");
+  $("#parrafo1").toggle("slow");
 })
 
 
 $("#titulo2").click(function () {
-    $("#parrafo2").toggle("slow");
+  $("#parrafo2").toggle("slow");
 })
 
 
 $("#titulo3").click(function () {
-    $("#parrafo3").toggle("slow");
+  $("#parrafo3").toggle("slow");
 })
 
 $("#titulo4").click(function () {
-    $("#parrafo4").toggle("slow");
-})
-
-
-
-
-$("#direccionarInicio").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#inicio").offset().top
-    }, 2000);
-});
-
-$("#direccionarQuienesSomos").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#quienesSomos").offset().top
-    }, 2000);
-});
-
-$("#direccionarDestacados").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#destacados").offset().top
-    }, 2000);
-});
-
-$("#direccionarContacto").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#contacto").offset().top
-    }, 2000);
-});
-
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+  $("#parrafo4").toggle("slow");
 })
